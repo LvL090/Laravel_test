67f330d5c2e119249e3107fcb0206c799759d480
Daniel C.A <calvod879@gmail.com>
2023-02-14 10:44:19 +0100

    factories users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Videogame;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,15 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+        //  \App\Models\User::factory()->create([
         //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //      'email' => 'test@example.com',
+        //  ]);
 
         $this->call([
             CategoriesTableSeeder::class,
         ]);
+
+       // Videogame::factory(100)->create();
     }
 }
